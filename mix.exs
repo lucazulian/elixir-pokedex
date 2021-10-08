436c569762d58e993e52b757faf886b2457b7f80
lucazulian <lucagiuggia@gmail.com>
2021-10-08 14:05:12 +0200

    test: add coverage and other libraries for testing purposes

--- mix.exs
+++ mix.exs
@@ -39,7 +39,9 @@ defmodule ElixirPokedex.MixProject do
       {:telemetry_poller, "~> 1.0"},
       {:gettext, "~> 0.18"},
       {:jason, "~> 1.2"},
-      {:plug_cowboy, "~> 2.5"}
+      {:plug_cowboy, "~> 2.5"},
+      {:coveralls, "~> 2.2"},
+      {:muzak, "~> 1.1"}
     ]
   end
 
