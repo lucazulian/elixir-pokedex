aa7ad2e45f24ca90a3e1eb50fdccb6f358d5bdb4
lucazulian <lucagiuggia@gmail.com>
2021-10-08 15:09:28 +0200

    ci: fix static analysis

--- mix.exs
+++ mix.exs
@@ -37,15 +37,20 @@ defmodule ElixirPokedex.MixProject do
       {:plug_cowboy, "~> 2.5"},
       {:excoveralls, "~> 0.10", only: [:dev, :test]},
       {:muzak, "~> 1.1"},
-      {:dialyxir, "~> 1.1", only: [:dev], runtime: false}
+      {:dialyxir, "~> 1.1", only: [:dev], runtime: false},
+      {:credo, "~> 1.5", only: [:dev], runtime: false}
     ]
   end
 
   defp aliases do
     [
+      check: [
+        "credo --strict",
+        "dialyzer --format dialyzer"
+      ],
+      cover: ["coveralls"],
       "format.all": "format mix.exs 'lib/**/*.{ex,exs}' 'test/**/*.{ex,exs}' 'config/*.{ex,exs}'",
-      setup: ["deps.get"],
-      cover: ["coveralls"]
+      setup: ["deps.get"]
     ]
   end
 
